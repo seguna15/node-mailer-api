6dcb2a4d890065eeeff225e82cb27bfb90efe657
Ubuntu <ubuntu@ip-172-31-85-168.ec2.internal>
2023-10-04 12:29:21 +0000

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
